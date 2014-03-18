537ec52de24c8fc81c323b551c289611448a869e
Grégoire Audoux <projects@gregoire-audoux.fr>
2014-03-18 12:29:17 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('BSMashup.Webapp')
+    .controller('MainCtrl', function ($scope, $resource) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
